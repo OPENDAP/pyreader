278bff8fe775f6653aad3d0e5ff92c836959fcac
Samuel Lloyd <slloyd@opendap.org>
2023-05-15 15:15:37 -0600

    first push of pyreader

---
+++ driver.py
@@ -0,0 +1,66 @@
+import get_s3_files
+import xml.dom.minidom as minidom
+import regex as re
+
+
+doc_template = """<?xml version="1.0" encoding="UTF-8"?>
+    <bes:request xmlns:bes="http://xml.opendap.org/ns/bes/1.0#" reqID="[http-nio-8080-exec-6:40:s3_request][bes_client:/-0]">
+      <bes:setContext name="bes_timeout">0</bes:setContext>
+      <bes:setContext name="dap_explicit_containers">no</bes:setContext>
+      <bes:setContext name="errors">xml</bes:setContext>
+      <bes:setContext name="max_response_size">0</bes:setContext>
+      <bes:setContainer name="S3_1" space="s3">
+        URL_TEMPLATE
+      </bes:setContainer>
+      <bes:define name="d1" space="default">
+        <bes:container name="S3_1" />
+      </bes:define>
+      <bes:get type="dap" definition="d1" />
+    </bes:request>
+"""
+
+
+def read_prefix_config():
+    # Using readlines()
+    file1 = open('prefixs.txt', 'r')
+    raw_prefixes = file1.readlines()
+    prefixes = []
+    count = 0
+
+    # Strips the newline character
+    for prefix in raw_prefixes:
+        count += 1
+        prefixes.append(prefix.strip())
+        # print("Line {}: {}".format(count, prefix.strip()))
+
+    return prefixes
+
+
+def create_bescmd(url):
+    pattern = "https:.*\/(.*).dmrpp"
+    match = re.search(pattern, url)
+    filename = match.group(1)
+
+    bescmd = doc_template.replace("URL_TEMPLATE", url)
+    root = minidom.parseString(bescmd)
+    # print(root.toprettyxml())
+    xml_str = root.toprettyxml(indent="\t", encoding="UTF-8")
+    save_path_file = "./bescmds/"+filename+".bescmd"
+    with open(save_path_file, "w+b") as f:
+        f.write(xml_str)
+
+
+def main():
+    import argparse  # for parsing arguments
+    prefixes = read_prefix_config()
+    for prefix in prefixes:
+        s3_list = get_s3_files.get_file_list(prefix)
+        print("--" + prefix + "--")
+        print(s3_list)
+        for s3_url in s3_list:
+            create_bescmd(s3_url)
+        print("----------")
+
+
+if __name__ == "__main__":
+    main()
---
+++ get_s3_files.py
@@ -0,0 +1,61 @@
+
+import requests
+import os
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+import xml.dom.minidom as minidom
+import regex as re
+
+
+def build_creds():
+    user = os.getenv('CMAC_ID')
+    password = os.environ.get('CMAC_ACCESS_KEY')
+    region = os.getenv('CMAC_REGION')
+
+    auth = AWSRequestsAuth(aws_access_key=user,
+                           aws_secret_access_key=password,
+                           aws_host='s3-module-test-bucket.s3.us-west-2.amazonaws.com',
+                           aws_region=region,
+                           aws_service='s3')
+
+    return auth
+
+
+def get_file_list(prefix):
+    s3_url = "https://s3-module-test-bucket.s3.us-west-2.amazonaws.com/"
+    # resp = requests.get(s3_url, auth=auth, params=param)
+    auth = build_creds()
+    if prefix != '':
+        param = {'prefix': prefix}
+        resp = requests.get(s3_url, auth=auth, params=param)
+    else:
+        resp = requests.get(s3_url, auth=auth)
+
+    # print(resp.status_code)
+    # print(resp.text)
+    # print("---------")
+
+    dom1 = minidom.parseString(resp.text)
+    keys = dom1.getElementsByTagName("Key")
+    url_list = []
+    for element in keys:
+        e = element.firstChild.nodeValue
+        # print(e)
+        r = re.compile('.*\.dmrpp')
+        if r.match(e):
+            full_url = s3_url + e
+            # print(full_url)
+            url_list.append(full_url)
+    # print("---------")
+
+    return url_list
+
+
+def main():
+    import argparse  # for parsing arguments
+    s3_list = get_file_list("test_folder")
+    print(s3_list)
+    # get_file_list("")
+
+
+if __name__ == "__main__":
+    main()
