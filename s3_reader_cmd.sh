870e71a3dc4c03369ce973a647bcbeb7fc8660dd
James Gallagher <jgallagher@opendap.org>
2023-06-01 10:03:21 -0600

    Added a conf file and script that use the dockerized bes

---
+++ s3_reader_cmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#
+
+if test -n "$1"
+then
+    cmd=$1
+else
+    echo "Usage: s3_reader_cmd <bes cmd file>"
+    exit 1
+fi
+
+docker exec --interactive --tty \
+--env CMAC_URL=https://s3-module-test-bucket.s3.us-west-2.amazonaws.com/ \
+--env CMAC_REGION=us-west-2 \
+--env CMAC_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+--env CMAC_ID=$AWS_ACCESS_KEY_ID \
+besd \
+besstandalone -c /usr/share/pyreader/pyreader-bes-docker.conf -i /usr/share/pyreader/$cmd
